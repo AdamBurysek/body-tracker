d1b2d9b997ef0fc2978e7af248be9df5a0df9f77
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-06-15 10:37:51 +0200

    Initial Commit

---
+++ BodyTracker/BodyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyTrackerApp.swift
+//  BodyTracker
+//
+//  Created by Adam Buryšek on 15.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct BodyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
