96f74a860a0f183a18833f3e9dc5872b105d8ef5
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-06-15 18:11:15 +0200

    add skeleton to view

--- BodyTracker/ARViewContainer.swift
+++ BodyTracker/ARViewContainer.swift
@@ -9,11 +9,17 @@ import SwiftUI
 import ARKit
 import RealityKit
 
+private var bodySkeleton: BodySkeleton?
+private let bodySkeletonAnchor = AnchorEntity()
+
 struct ARViewContainer: UIViewRepresentable {
     typealias UIViewType = ARView
         
     func makeUIView(context: Context) -> ARView {
         let arView = ARView (frame: .zero, cameraMode: .ar, automaticallyConfigureSession: true)
+        
+        arView.setupForBodyTracking()
+        arView.scene.addAnchor(bodySkeletonAnchor)
         
         return arView
     }
@@ -23,3 +29,28 @@ struct ARViewContainer: UIViewRepresentable {
     }
     
 }
+
+extension ARView: ARSessionDelegate {
+    func setupForBodyTracking() {
+        let configuration  = ARBodyTrackingConfiguration()
+        self.session.run(configuration)
+        
+        self.session.delegate = self
+    }
+    
+    public func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        for anchor in anchors {
+            if let bodyAnchor = anchor as? ARBodyAnchor {
+                if let skeleton = bodySkeleton {
+                    // BodySkeleton already exists, update all joints and bones
+                    skeleton.update(with: bodyAnchor)
+                } else {
+                    // BodySkeleton doesen't yet exist. This means a body has been detected for thi first time.
+                    // Create bodySkeleton entity and add it to the bodySkeletonAnchor
+                    bodySkeleton = BodySkeleton(for: bodyAnchor)
+                    bodySkeletonAnchor.addChild(bodySkeleton!)
+                }
+            }
+        }
+    }
+ }
